1ced2568df22fc8ee568261abe8b47ac43465c0d
Mike Yin <yinjitong@hotmail.com>
2020-04-27 20:16:29 -0400

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB, { useNewUrlParser: true })
   .then(() => console.log(`Database connected successfully`))
