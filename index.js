3e193ea8facde803ddf5f8ccf3b3dd34d2a3705f
Mike Yin <yinjitong@hotmail.com>
2020-04-28 10:15:06 -0400

    remove log

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-console.log(process.env.DB);
 mongoose.connect(process.env.DB, { useNewUrlParser: true })
   .then(() => console.log(`Database connected successfully`))
   .catch(err => console.log(err));
